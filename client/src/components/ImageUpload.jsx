ed4f59973c8db18f8c08bdb2c7dcf2b83e2c8567
Shubhankar_Ranjan <shubhankar515@gmail.com>
2024-02-28 01:04:39 +0530

    edit input field

--- client/src/components/ImageUpload.jsx
+++ client/src/components/ImageUpload.jsx
@@ -1,23 +1,28 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { AiOutlineClose } from 'react-icons/ai';
+import { MdOutlineAddPhotoAlternate } from "react-icons/md";
 import Title from './Title';
 
 const ImageUpload = () => {
     const [file, setFile] = useState(null);
     const [result, setResult] = useState(null);
     const [preview, setPreview] = useState(null);
+    const [dragging, setDragging] = useState(false);
+    const [imageUploaded, setImageUploaded] = useState(false);
 
-    const handleFileChange = async (event) => {
-        setFile(event.target.files[0]);
+    const handleFileChange = async (file) => {
+        setFile(file);
 
         // Create a preview of the image
-        const url = URL.createObjectURL(event.target.files[0]);
+        const url = URL.createObjectURL(file);
         setPreview(url);
+
+        setImageUploaded(true);
 
         // Automatically submit the form
         const formData = new FormData();
-        formData.append('file', event.target.files[0]);
+        formData.append('file', file);
 
         const response = await axios.post('http://127.0.0.1:8000/predict', formData, {
             headers: {
@@ -33,35 +38,65 @@ const ImageUpload = () => {
         setResult(response.data);
     };
 
+    const handleDragOver = (event) => {
+        event.preventDefault();
+        setDragging(true);
+    };
+
+    const handleDragLeave = () => {
+        setDragging(false);
+    };
+
+    const handleDrop = (event) => {
+        event.preventDefault();
+        setDragging(false);
+
+        if (event.dataTransfer.items && event.dataTransfer.items[0]) {
+            handleFileChange(event.dataTransfer.items[0].getAsFile());
+        }
+    };
+
     const removeImage = () => {
         setFile(null);
         setPreview(null);
         setResult(null);
+        setImageUploaded(false);
     };
 
     return (
         <div className="flex flex-col h-screen bg-gray-200 p-8 space-y-8 overflow-hidden">
             <Title />
-            <form className="flex flex-col items-center justify-center border-4 border-dashed border-gray-500 rounded-lg p-8 text-center space-y-4 bg-gray-100 shadow-lg">
-                <input type="file" onChange={handleFileChange} className="py-2 px-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent" />
-                <em className="text-sm text-gray-500">(Only *.jpeg and *.jpg images will be accepted)</em>
+            <form className={`flex flex-col items-center justify-center border-4 ${dragging ? 'border-blue-500' : 'border-dashed border-gray-500'} rounded-lg p-8 text-center space-y-4 bg-gray-100 shadow-lg`}
+                onDragOver={handleDragOver}
+                onDragLeave={handleDragLeave}
+                onDrop={handleDrop}
+            >
+                {!imageUploaded && (
+                    <>
+                        <input type="file" id="file-input" onChange={(event) => handleFileChange(event.target.files[0])} className="hidden" />
+                        <label htmlFor="file-input" className="cursor-pointer flex flex-col items-center justify-center">
+                            <MdOutlineAddPhotoAlternate size={100} />
+                            <p>Drag and drop a file, or click to select a file</p>
+                        </label>
+                    </>
+                )}
+                <div className="flex justify-center items-center space-x-4 mt-8">
+                    {preview && (
+                        <div className="relative">
+                            <img src={preview} alt="Preview" className="h-64" />
+                            <button onClick={removeImage} className="absolute -top-2 -right-2 p-1 hover:bg-gray-200 active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+                                <AiOutlineClose size={24} color="red" />
+                            </button>
+                        </div>
+                    )}
+                    {result && (
+                        <div className="p-4 bg-white rounded shadow-lg text-center">
+                            <p className="text-xl font-bold text-gray-700">Class: {result.Class}</p>
+                            <p className="text-xl font-bold text-gray-700">Confidence: {result.Confidence}</p>
+                        </div>
+                    )}
+                </div>
             </form>
-            <div className="flex justify-center items-center space-x-4 mt-8">
-                {preview && (
-                    <div className="relative">
-                        <img src={preview} alt="Preview" className="h-64" />
-                        <button onClick={removeImage} className="absolute -top-2 -right-2 p-1 hover:bg-gray-200 active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
-                            <AiOutlineClose size={24} color="red" />
-                        </button>
-                    </div>
-                )}
-                {result && (
-                    <div className="p-4 bg-white rounded shadow-lg text-center">
-                        <p className="text-xl font-bold text-gray-700">Class: {result.Class}</p>
-                        <p className="text-xl font-bold text-gray-700">Confidence: {result.Confidence}</p>
-                    </div>
-                )}
-            </div>
         </div>
     );
 };
